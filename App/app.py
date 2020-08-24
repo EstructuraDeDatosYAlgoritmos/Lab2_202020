0b696af250a0318f967f71ececc41a7230b3e5d7
Kevin <k.moralesc@uniandes.edu.co>
2020-08-24 10:08:19 -0500

    CAMBIO DE FUNCION SORT

--- App/app.py
+++ App/app.py
@@ -27,14 +27,14 @@
   datos, contar elementos, y hacer búsquedas sobre una lista.
 """
 
-import config as cf
-import sys
-import csv
 from DataStructures import arraylist as adt
-from time import process_time
 from App.comparation import Comparation
 from App.comparation import cmpfunction
-from Sorting.insertionsort import insertionSort
+from Sorting.mergesort import mergesort
+from time import process_time
+import App.config as cf
+import sys
+import csv
 
 def loadCSVFile (data_link, data, sep=";"):
     """
@@ -85,35 +85,6 @@ def loadCSVFile (data_link, data, sep=";"):
     t1_stop = process_time()  #tiempo final
     print("Tiempo de ejecución ",t1_stop-t1_start," segundos")
 
-def loadCSVFile1(archivo:str)->list:
-    moviedetails = []
-    
-    archivo = open(archivo, "r")
-    archivo.readline()
-    linea = archivo.readline()
-   
-    while len(linea) > 0:
-        
-        lin=linea.split(";")
-        moviedetails.append({"id":lin[0], "budget":lin[1],"genres":lin[2],"imdb_id":lin[3],"original_language":lin[4],"original_title":lin[5],"overview":lin[6],"popularity":lin[7],"nameproduction_companies":lin[8],"nameproduction_countries":lin[9],"release_date":lin[10],"revenue":lin[11],"runtime":lin[12],"namespoken_languages":lin[13],"status":lin[14],"tagline":lin[15],"title":lin[16],"vote_average":lin[17],"vote_count":lin[18],"numproduction_companies":lin[19],"numproduction_countries":lin[20],"numspoken_languages":lin[21]})
-        linea = archivo.readline();
-    archivo.close    
-    return moviedetails
-
-def loadCSVFile2 (archivo:str)->list:
-    moviecasting = []
-    
-    archivo = open(archivo, "r")
-    archivo.readline()
-    linea = archivo.readline()
-   
-    while len(linea) > 0:
-        
-        lin=linea.split(";")
-        moviecasting.append({"id":lin[0], "actor1_name":lin[1],"actor1_gender":lin[2],"actor2_name":lin[3],"actor2_gender":lin[4],"actor3_name":lin[5],"actor3_gender":lin[6],"actor4_name":lin[7],"actor4_gender":lin[8],"actor5_name":lin[9],"actor5_gender":lin[10],"actor_number":lin[11],"director_name":lin[12],"director_gender":lin[13],"director_number":lin[14],"producer_name":lin[15],"producer_number":lin[16],"screenplay_name":lin[17],"editor_name":lin[18]})
-        linea = archivo.readline();
-    archivo.close    
-    return moviecasting
 
 
 def printMenu():
@@ -191,29 +162,16 @@ def countElementsByCriteria(data, director):
     
     return (counter,average)
 
-def countElementsByCriteria1(moviedetails: list, moviecasting: list, director_name: str)->tuple:
-     buenas = 0
-     promedio = 0
-     peliculas_d = 0                        
-     for b in moviecasting:
-        if b['director_name']==director_name: 
-            peliculas_d+=1
-            promedio += a['vote_average']
-            promedio = promedio/peliculas_d
-            for a in moviedetails:
-                if a['vote_average']>= 6:
-                    buenas+=1
-                
-                             
-     resultado = (buenas, promedio)             
-     return resultado
 
 def orderElementsByCriteria(data,less):
-    insertionSort(data, less)
-    ranking = []
-    for i in range(1,11):
-        ranking.append(adt.getElement(data, i))
-    return ranking
+    if adt.size(data)==0:
+        print("La lista esta vacía")
+    else:
+        mergesort(data, less)
+        ranking = []
+        for i in range(1,11):
+            ranking.append(adt.getElement(data, i))
+        return ranking
 
 def main():
     """
